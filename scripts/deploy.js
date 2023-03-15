d61b8afdc26c4a228e03950e9723af0ed04a6060
moheet333 <moheetshendarkar@gmail.com>
2023-03-15 13:30:23 +0530

    eth

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,6 +5,7 @@ const tokens = (n) => {
 }
 
 async function main() {
+<<<<<<< Updated upstream
   // Setup accounts & variables
   const [deployer] = await ethers.getSigners()
   const NAME = "Dappcord"
@@ -26,6 +27,24 @@ async function main() {
     await transaction.wait()
 
     console.log(`Created text channel #${CHANNEL_NAMES[i]}`)
+=======
+  const Dappcord = await hre.ethers.getContractFactory("Dappcord");
+  const dappcord = await Dappcord.deploy("Dappcord", "DC");
+  await dappcord.deployed();
+  const [deployer, random] = await hre.ethers.getSigners();
+
+  console.log("Deployed contract at : ", dappcord.address);
+
+  const channel_names = ["general", "intro", "jobs"];
+  const costs = [tokens(1), tokens(0), tokens(0.25)];
+  // create 3 channels
+  for (var i = 0; i < 3; i++) {
+    let txn = await dappcord
+      .connect(deployer)
+      .createChannel(channel_names[i], costs[i]);
+    await txn.wait();
+    console.log(`Channel created #${channel_names[i]}`);
+>>>>>>> Stashed changes
   }
 }
 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
-import { io } from "socket.io-client"
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import { io } from "socket.io-client";
 
 // Components
-import Navigation from './components/Navigation'
-import Servers from './components/Servers'
-import Channels from './components/Channels'
-import Messages from './components/Messages'
+import Navigation from "./components/Navigation";
+import Servers from "./components/Servers";
+import Channels from "./components/Channels";
+import Messages from "./components/Messages";
 
 // ABIs
-import Dappcord from './abis/Dappcord.json'
+import Dappcord from "./abis/Dappcord.json";
 
 // Config
-import config from './config.json';
+import config from "./config.json";
 
 // Socket
-const socket = io('ws://localhost:3030');
+const socket = io("ws://localhost:3030");
 
 function App() {
+<<<<<<< Updated upstream
   const [provider, setProvider] = useState(null)
   const [account, setAccount] = useState(null)
 
@@ -73,6 +74,19 @@ function App() {
       socket.off('get messages')
     }
   }, [])
+=======
+  const [account, setAccount] = useState(null);
+
+  const loadBlockchainData = async () => {
+    window.ethereum.on("accountsChanged", async () => {
+      window.location.reload();
+    });
+  };
+
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
+>>>>>>> Stashed changes
 
   return (
     <div>
@@ -80,6 +94,7 @@ function App() {
 
       <main>
         <Servers />
+<<<<<<< Updated upstream
 
         <Channels
           provider={provider}
@@ -91,6 +106,10 @@ function App() {
         />
 
         <Messages account={account} messages={messages} currentChannel={currentChannel} />
+=======
+        <Channels />
+        <Messages />
+>>>>>>> Stashed changes
       </main>
     </div>
   );
